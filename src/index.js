f7e7782918c8039b93abf0ef2c36351f267f76b7
Guilherme Fugazza <104568676+GuilhermeFugazza@users.noreply.github.com>
2023-06-13 17:50:25 +0000

    Criando conexão com o BD

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 const express = require("express")
 const fs = require("fs")
 const app = express()
+const { getDatabaseInstance } = require("./database")
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/create", (req, res) => {
-  const { file, text } = req.query
-  fs.writeFileSync(file, text)
-  res.send(console.log("Arquivo CRIADO com sucesso!")) 
+app.use("/create", async (req, res) => {
+  const { title, source, description, thumb } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(`
+    INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`,
+    [title, source, description, thumb]
+  )
+  res.send(result)
 })
 
-app.use("/read", (req, res) => {
-  const { file } = req.query
-  const text = fs.readFileSync(file)
-  res.send(text.toString())
+app.use("/read", async (req, res) => {
+  const { id } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.get(`
+    SELECT * FROM movies WHERE id=?`, 
+    [id]
+  )
+  res.send(result)
 })
 
-app.use("/update", (req, res) => {
-  const { file, text } = req.query
-  fs.appendFileSync(file, text)
-  res.send(console.log("Arquivo ATUALIZADO com sucesso!"))
+app.use("/update", async (req, res) => {
+  const { id, title, source, description, thumb } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(`
+    UPDATE movies SET title=?, source=?, description=?, thumb=? WHERE id=?`, 
+    [title, source, description, thumb, id]
+  )
+  res.send(result)
 })
 
-app.use("/delete", (req, res) => {
-  const { file } = req.query
-  fs.rmSync(file) 
-  res.send(console.log("Arquivo EXCLUÍDO com sucesso!"))
+app.use("/delete", async (req, res) => {
+  const { id } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(`
+    DELETE FROM movies WHERE id=?`, 
+    [id]
+  )
+  res.send(result)
 })
 
 app.listen(3000, () => console.log("server running!"))
